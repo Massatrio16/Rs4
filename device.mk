d6009bff1d181a31bfb57141eec360c4243a3f0e
Massatrio16 <146039181+Massatrio16@users.noreply.github.com>
2024-05-12 04:15:33 +0700

    Update device.mk

--- device.mk
+++ device.mk
@@ -10,22 +10,14 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-# Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit_only.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
+LOCAL_PATH := device/itel/S666LN
 
-# Installs gsi keys into ramdisk, to boot a developer GSI with verified boot.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
-
-# Enable project quotas and casefolding for emulated storage without sdcardfs
-$(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
 
 # Enable Virtual A/B OTA
 $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/launch_with_vendor_ramdisk.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/compression.mk)
 
 
-LOCAL_PATH := device/itel/S666LN
 
 ENABLE_VIRTUAL_AB := true
 AB_OTA_UPDATER := true
